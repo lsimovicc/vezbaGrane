e7d8b0c5aa8f635f3e45a04c10b4f59874ecef8a
lsimovicc <lazar.simovic@logeecom.com>
2025-04-29 16:18:30 +0200

    dodate kategorije

---
+++ VezbaGrane/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿
+
+using DemoProject.Data.Model;
+using DemoProject.Dto;
+
+namespace DemoProject.Mappings;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<CategoryDto, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.SubCategories, opt => opt.Ignore());
+    }
+}
---
+++ VezbaGrane/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DemoProject.Dto;
+
+public class CategoryDto
+{ 
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required string Code { get; set; }
+        
+        public Guid? ParentCategory { get; set; }
+}
